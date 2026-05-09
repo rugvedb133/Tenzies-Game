b31c28017ac2be257fb9c7828a65454595b8dc0d
rugvedb133 <rugvedbhise@gmail.com>
2026-05-09 17:16:45 +0530

    Use React states - use React states - set die values from auto-generated random numbers - display these values as button components

--- App.jsx
+++ App.jsx
@@ -1,45 +1,36 @@
-import Die from "./Die"
+import { useState } from "react"
+import Die from "./components/Die"
 
 /**
  * Challenge:
- * 
- * Write a function (generateAllNewDice) that returns an array 
+ *
+ * Write a function (generateAllNewDice) that returns an array
  * of 10 random numbers between 1-6 inclusive.
- * 
+ *
  * Log the array of numbers to the console for now
  */
 
-function generateDieValue() {
-    return Math.floor(Math.random() * 6) + 1
-}
-// console.log(generateDieValue())
-
-let diceArray=[]
-
-function generateAllNewDice(diceArray) {
-    for (let i=0; i<10; i++){
-        diceArray[i] = generateDieValue()    
-    }
-    return diceArray
-}
-diceArray = generateAllNewDice(diceArray)
-console.log(diceArray)
-
 export default function App() {
-    return (
-        <main>
-            <div className="dice-container">
-                <Die value={1} />
-                <Die value={2} />
-                <Die value={3} />
-                <Die value={4} />
-                <Die value={5} />
-                <Die value={6} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-            </div>
-        </main>
-    )
+  function generateDieValue() {
+    return Math.floor(Math.random() * 6) + 1;
+  }
+
+  const [diceArray, setDiceArray] = useState(generateAllNewDice());
+
+  function generateAllNewDice() {
+    let diceArray = []
+    for (let i = 0; i < 10; i++) {
+      diceArray[i] = generateDieValue();
+    }
+    return diceArray;
+  }
+
+  const diceComponents = diceArray.map(die => <Die value={die} />)
+  return (
+    <main>
+      <div id="dice-container">
+        {diceComponents}
+      </div>
+    </main>
+  );
 }
