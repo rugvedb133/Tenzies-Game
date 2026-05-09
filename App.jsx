ac31e5ab91c07e24ffe3104cc45cff392bb572ce
rugvedb133 <rugvedbhise@gmail.com>
2026-05-09 17:02:47 +0530

    Die values - Helper function to generate random value between 1 and 6 - Function to generate initial array filled with random values

--- App.jsx
+++ App.jsx
@@ -1,35 +1,45 @@
+import Die from "./Die"
+
 /**
  * Challenge:
  * 
- * - Create a Die component that takes a `value` prop. Should
- *   render a button with that value displayed.
- * - Render 10 instances of the Die component (manually)
- *      - Provide a number between 1-6 for the value on each
- *        for now
- * - Style the <main> and <Die> components 
- *   to look like they do in the slide
- *      - Hints: Create a container to hold the 10 instances
- *        of the Die component, and use CSS Grid to lay them
- *        out evenly in 2 rows of 5 columns
- *      - Use flexbox on main to center the dice container
- *        in the center of the page
+ * Write a function (generateAllNewDice) that returns an array 
+ * of 10 random numbers between 1-6 inclusive.
+ * 
+ * Log the array of numbers to the console for now
  */
 
-import Die from "./components/Die"
+function generateDieValue() {
+    return Math.floor(Math.random() * 6) + 1
+}
+// console.log(generateDieValue())
+
+let diceArray=[]
+
+function generateAllNewDice(diceArray) {
+    for (let i=0; i<10; i++){
+        diceArray[i] = generateDieValue()    
+    }
+    return diceArray
+}
+diceArray = generateAllNewDice(diceArray)
+console.log(diceArray)
 
 export default function App() {
-    return (<main>
-        <div id="dice-container">
-            <Die value="1" />
-            <Die value={2} />
-            <Die value={3} />
-            <Die value={4} />
-            <Die value={5} />
-            <Die value={6} />
-            <Die value={7} />
-            <Die value={8} />
-            <Die value={9} />
-            <Die value={10} />
-        </div>
-    </main>)
+    return (
+        <main>
+            <div className="dice-container">
+                <Die value={1} />
+                <Die value={2} />
+                <Die value={3} />
+                <Die value={4} />
+                <Die value={5} />
+                <Die value={6} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+                <Die value={1} />
+            </div>
+        </main>
+    )
 }
